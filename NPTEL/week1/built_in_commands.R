98665198132566c3acd15d922056a7bc29edbde0
Sowmya L R <sowmyasonait@gmail.com>
2022-01-27 12:24:13 +0530

    nptel files for R programming language

---
+++ NPTEL/week1/built_in_commands.R
@@ -0,0 +1,13 @@
+# Finding max elements
+
+max(2.3,100,-187, 200,300,10,600)
+max(c(2.3,100,-187, 200,300,10,600))
+min(2.3,100,-187, 200,300,10,600)
+min(c(2.3,100,-187, 200,300,10,600))
+sqrt(c(9,16))
+# exp -> exponential e^y
+exp(c(3,4))
+x = c(1,2,3,4)
+y = c(2,4,6,8)
+plot(x,y,"b", main="Overall title", sub="subtitle", xlab="x axis", ylab="y axis")
+
---
+++ NPTEL/week1/sampling.R
@@ -0,0 +1,4 @@
+x <- 1:20
+
+y = sample(x, 10, replace=TRUE)
+y
